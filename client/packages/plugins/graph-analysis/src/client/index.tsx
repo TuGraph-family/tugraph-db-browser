2a06902907f5e9dbc69398cc3705552d70f5396e
聚则 <zhanning.bzn@antgroup.com>
2023-08-03 15:36:13 +0800

    feat: 添加图分析路由菜单

---
+++ client/packages/plugins/graph-analysis/src/client/index.tsx
@@ -0,0 +1,75 @@
+import React, { useContext } from 'react';
+import { TableOutlined } from '@ant-design/icons';
+import {
+  SchemaComponentOptions,
+  SchemaInitializer,
+  SchemaInitializerContext,
+} from '@tugraph/openpiece-client';
+import { PluginDesigner } from './PluginDesigner';
+
+const GraphAnalysis = () => {
+  return (
+    <div>图分析</div>
+  )
+}
+
+export const PluginBlockInitializer = (props) => {
+  const { insert } = props;
+
+  const schema = {
+    type: 'void',
+    'x-component': 'CardItem',
+    'x-designer': 'PluginDesigner',
+    properties: {
+      row1: {
+        type: 'void',
+        'x-component': 'GraphAnalysis',
+        'x-async': false,
+        'x-index': 1,
+        'x-component-props': {},
+      },
+    },
+  };
+
+  return (
+    <SchemaInitializer.Item
+      {...props}
+      icon={<TableOutlined />}
+      onClick={() => {
+        insert(schema);
+      }}
+      title="GraphAnalysis"
+    />
+  );
+};
+
+export default React.memo((props) => {
+  const items = useContext(SchemaInitializerContext) as any;
+  const children = items?.BlockInitializers?.items?.[1]?.children ?? [];
+
+  const hasCustomBlock = children?.find(
+    (d) => d.key === ' GraphAnalysis'
+  );
+
+  if (!hasCustomBlock) {
+    children.push({
+      key: ' GraphAnalysis',
+      type: 'item',
+      title: ' GraphAnalysis',
+      component: PluginBlockInitializer,
+    });
+  }
+  return (
+    <SchemaComponentOptions
+      components={{
+        PluginDesigner,
+        PluginBlockInitializer,
+        GraphAnalysis,
+      }}
+    >
+      <SchemaInitializerContext.Provider value={items}>
+        {props.children}
+      </SchemaInitializerContext.Provider>
+    </SchemaComponentOptions>
+  );
+});
