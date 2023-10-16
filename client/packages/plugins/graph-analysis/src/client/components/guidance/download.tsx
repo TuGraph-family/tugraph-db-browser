e1f418ffea4d723b34744d434300c5125a734596
逍为 <xiaowei.wzw@antgroup.com>
2023-10-16 14:28:12 +0800

    feat: guidance logical

--- client/packages/plugins/graph-analysis/src/client/components/guidance/download.tsx
+++ client/packages/plugins/graph-analysis/src/client/components/guidance/download.tsx
@@ -9,9 +9,9 @@ import styles from './index.module.less';
  * 新手引导 - 下载
  * @returns 
  */
-export const Download: React.FC<StepUIProps> = ({ prev, next, end }) => {
+export const Download: React.FC<StepUIProps> = ({ prev, next, end, x, y }) => {
   return (
-    <div className={styles.download} style={{ right: 36, top: 90 }}>
+    <div className={styles.download} style={{ right: 8, top: y }}>
       <div className={styles.container}>
         <img className={styles.background} style={{ width: 360 }} src={DownloadImage} />
         <div className={styles.btnContainer}>
@@ -20,7 +20,7 @@ export const Download: React.FC<StepUIProps> = ({ prev, next, end }) => {
             <div className={styles.nextText} onClick={next}>下一步</div>
           </div>
         </div>
-        <div className={styles.close} style={{ top: 24 }} onClick={end}>
+        <div className={styles.close} style={{ top: 24, right: 'unset', left: -32 }} onClick={end}>
           <img src={CloseImage} />
         </div>
       </div>
--- client/packages/plugins/graph-analysis/src/client/components/guidance/index.tsx
+++ client/packages/plugins/graph-analysis/src/client/components/guidance/index.tsx
@@ -1,7 +1,5 @@
-import React, { useState, useMemo, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocalStorageState } from 'ahooks';
-import { Driver, Step } from '../driver';
-
 import { Welcome } from './welcome';
 import { Query } from './query';
 import { Spread } from './spread';
@@ -12,14 +10,30 @@ import { End } from './end';
 
 import styles from './index.module.less';
 
-const Steps = [
-  { element: '.page-header', popover: { title: 'Title', description: 'Description' } },
-  { element: '.top-nav', popover: { title: 'Title', description: 'Description' } },
-  { element: '.sidebar', popover: { title: 'Title', description: 'Description' } },
-  { element: '.footer', popover: { title: 'Title', description: 'Description' } },
-];
+const LocalStorageKey = '__guidance_key__';
+/**
+  一些组件的 class selector。
+  查询：gi-richcontainer-query-button
+  筛选：gi-richcontainer-filter-button
+  样式：gi-richcontainer-stylesetting-button
+  下载按钮：tugraph-download-container
+  左侧面板：gi-richcontainer-side
+  GI 画布：gi-rich-container-canvas
+  */
+const SelectorQuery = '.gi-richcontainer-query-button';
+const SelectorFilter = '.gi-richcontainer-filter-button';
+const SelectorStyle = '.gi-richcontainer-stylesetting-button';
+const SelectorDownload = '.tugraph-download-container';
+const SelectorSidebar = '.gi-richcontainer-side';
+const SelectorCanvas = '.gi-rich-container-canvas';
 
-const LocalStorageKey = '__guidance_key__'
+function getElement(selector: string): HTMLElement {
+  return document.querySelector(selector);
+}
+
+function getBoundings(selector: string): DOMRect {
+  return getElement(selector).getBoundingClientRect();
+}
 
 /**
  * 新手引导组件
@@ -29,37 +43,19 @@ export const Guidance: React.FC = () => {
   // localStorage 存储
   const [isGuidanceFinished, setGuidanceFinished] = useLocalStorageState<string>(LocalStorageKey);
 
-  const driver = useRef(null);
   const [step, setStep] = useState<number>(0);
-
-  const onStepChange = useMemo(() => {
-    return (step: Step, idx: number, steps: Step[]) => {
-      setStep(idx);
-    };
-  }, []);
-
-  // 初始化新手引导的 driver
-  useEffect(() => {
-    driver.current = new Driver(Steps, onStepChange);
-    // 如果没有阅读过新手引导（localStorage），启动新手引导
-    setStep(0);
-  }, []);
 
   // step 变化的时候，需要根据 step 自动打开对应的 dom 节点。
   useEffect(() => {
-    /**
-      查询：gi-richcontainer-query-button
-      筛选：gi-richcontainer-filter-button
-      样式：gi-richcontainer-stylesetting-button
-      下载按钮：tugraph-download-container
-      左侧面板：gi-richcontainer-side
-     */
     if (step === 1) {
       // 查询
+      getElement(SelectorQuery).click();
     } else if (step === 3) {
       // 样式
+      getElement(SelectorStyle).click();
     } else if (step === 5) {
       // 筛选
+      getElement(SelectorFilter).click();
     }
   }, [step]);
 
@@ -68,20 +64,51 @@ export const Guidance: React.FC = () => {
     next: () => setStep(step + 1),
     end: () => {
       setStep(-1);
-      setGuidanceFinished('1');
+      // todo 便于本地开发测试
+      // setGuidanceFinished('1');
     },
+  }
+
+  function getGuidanceUI() {
+    if (step === 0) return (<Welcome {...props} />);
+    if (step === 1) {
+      const rect = getBoundings(SelectorSidebar);
+      return (<Query {...props } x={rect.right - 16} y={rect.top + 100} />);
+    }
+    if (step === 2) {
+      const rect = getBoundings(SelectorCanvas);
+      return (<Spread {...props } x={rect.x + rect.width * 0.382} y={300} />);
+    }
+    if (step === 3) {
+      const rect = getBoundings(SelectorStyle);
+      // 32px 是箭头的偏移
+      return (<StyleBtn {...props } x={rect.x + rect.width / 2 - 32} y={rect.bottom} />);
+    }
+    if (step === 4) {
+      const rect = getBoundings(SelectorSidebar);
+      return (<StylePanel {...props } x={rect.right - 16} y={rect.top + 200} />);
+    }
+    if (step === 5) {
+      const rect = getBoundings(SelectorFilter);
+      // 32px 是箭头的偏移
+      return (<FilterBtn {...props } x={rect.x + rect.width / 2 - 32} y={rect.bottom} />);
+    }
+    if (step === 6) {
+      const rect = getBoundings(SelectorSidebar);
+      return (<FilterPanel {...props }  x={rect.right - 16} y={rect.top + 100} />);
+    }
+    if (step === 7) {
+      const rect = getBoundings(SelectorDownload);
+      return (<Download {...props } x={rect.x + rect.width / 2} y={rect.bottom} />);
+    }
+    if (step === 8) {
+      return (<End {...props } />);
+    }
+    return null;
   }
 
   return !isGuidanceFinished && 
     <div className={styles.guidance} style={{ display: step >= 0 && step <= 8 ? 'unset' : 'none' }}>
-      { step === 0 && <Welcome {...props} /> }
-      { step === 1 && <Query {...props } x={300} y={100} /> }
-      { step === 2 && <Spread {...props } x={800} y={200} /> }
-      { step === 3 && <StyleBtn {...props } x={465} y={90} /> }
-      { step === 4 && <StylePanel {...props } x={300} y={200} /> }
-      { step === 5 && <FilterBtn {...props } x={300} y={90} /> }
-      { step === 6 && <FilterPanel {...props } x={300} y={200} /> }
-      { step === 7 && <Download {...props } /> }
-      { step === 8 && <End {...props } /> }
+      { getGuidanceUI() }
     </div>;
 };
