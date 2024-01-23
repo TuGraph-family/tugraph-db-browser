da18c8a61794c0560460bff487ba1f66f326339b
qiming <138841683+noneAuth@users.noreply.github.com>
2024-01-23 18:55:38 +0800

    fix: login error method (#65)
    * fix: login error
    
    * fix: login error update code

--- client/src/components/studio/utils/request.tsx
+++ client/src/components/studio/utils/request.tsx
@@ -3,6 +3,8 @@ import { extend } from 'umi-request';
 import { PROXY_HOST } from '../constant';
 import { getLocalData } from './localStorage';
 
+export const HOLD_TIME = 2.5;
+
 const request = extend({
   headers: {
     'Content-Type': 'application/json',
@@ -17,14 +19,21 @@ const request = extend({
 // 中间件
 request.interceptors.response.use(async response => {
   const data = await response.clone().json();
-  if (data.errorCode == 401 || data.errorCode == 400) {
-    message.warning('登录过期，请重新登录');
-    // 清除掉之前的缓存
-    localStorage.removeItem('TUGRAPH_TOKEN');
-    window.location.href = '/login';
+  const TUGRAPH_TOKEN = getLocalData('TUGRAPH_TOKEN');
+  if ((!TUGRAPH_TOKEN && data.errorCode == 400) || data.errorCode == 401) {
+    new Promise<void>(resolve => {
+      setTimeout(() => {
+        message.warning('登录过期，请重新登录', HOLD_TIME);
+        resolve();
+      }, 2500);
+    }).then(() => {
+      // 清除掉之前的缓存
+      localStorage.removeItem('TUGRAPH_TOKEN');
+      window.location.href = '/login';
+    });
   }
   if (data.errorCode == 500) {
-    message.error('请求失败' + data.errorMessage);
+    message.error('请求失败' + data.errorMessage, HOLD_TIME);
   }
   return response;
 });
