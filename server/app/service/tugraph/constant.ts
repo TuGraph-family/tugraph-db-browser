b8d202f697b366f40945c87478d2786fc07ece6a
noneAuth <wb-zqm915773@antgroup.com>
2024-03-15 16:39:29 +0800

    feat: Add pre selected values for adaptive environment variables

--- server/app/service/tugraph/constant.ts
+++ server/app/service/tugraph/constant.ts
@@ -3,5 +3,9 @@ import { GetEnvironmentVariables } from '../../util';
 log(GetEnvironmentVariables(process.env), 'server');
 const HOST = GetEnvironmentVariables(process.env);
 export const HOST_URL = `http://47.105.42.70:9090`;
-
-export const EngineServerURL = `${HOST?.tugraph_db_host}/LGraphHttpService/Query`;
+export const DEV_HOST_URL = 'http://127.0.0.1:9090';
+export const EngineServerURL = `${
+  HOST?.tugraph_db_host ||
+  (process.env?.NODE_ENV === 'production' ? DEV_HOST_URL : HOST_URL)
+}/LGraphHttpService/Query`;
+log(EngineServerURL, 'eng');
