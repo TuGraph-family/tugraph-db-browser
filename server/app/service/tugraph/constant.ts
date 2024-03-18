243b09080605fc55504278e7e1259ddb6977be98
noneAuth <wb-zqm915773@antgroup.com>
2024-03-18 16:56:06 +0800

    feat: add env.json

--- server/app/service/tugraph/constant.ts
+++ server/app/service/tugraph/constant.ts
@@ -1,11 +1,11 @@
-import { log } from 'console';
 import { GetEnvironmentVariables } from '../../util';
-log(GetEnvironmentVariables(process.env), 'server');
+import env from '../../../../env.json';
 const HOST = GetEnvironmentVariables(process.env);
+
 export const HOST_URL = `http://47.105.42.70:9090`;
 export const DEV_HOST_URL = 'http://127.0.0.1:9090';
 export const EngineServerURL = `${
+  env?.tugraph_db_host ||
   HOST?.tugraph_db_host ||
   (process.env?.NODE_ENV === 'production' ? DEV_HOST_URL : HOST_URL)
 }/LGraphHttpService/Query`;
-log(EngineServerURL, 'eng');
